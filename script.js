7d3088ab25628e34d5db6a3fda6b73494c644940
Pawan <pawankumarpk3610@gmail.com>
2024-06-18 10:28:27 +0530

    Web Page to check odd or even

---
+++ script.js
@@ -0,0 +1,29 @@
+/*author - Pawan Kumar */
+
+const handleInput = () => {
+   const userInput = document.getElementById('userInput');
+   let inputValue = userInput.value;
+   inputValue = parseInt(inputValue);
+
+   if (isNaN(inputValue)) {
+      alert("Please Enter a Valid Number.");
+      return;
+   }
+
+   let message;
+      if (inputValue == 0) {
+         message = "Entered Number is Zero"
+      }
+      else if (inputValue%2 == 0) {
+         message = "Entered Number is Even"
+      }
+      else {
+         message = "Entered Number is Odd"
+
+      }
+
+   const outputElement = document.getElementById('output');
+   outputElement.innerHTML = message;
+
+   userInput.value = '';
+}
